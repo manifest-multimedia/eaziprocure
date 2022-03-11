ce06578de5001423b3d61a6a515837f19a3ffa9f
Johnson Sebire <johnson@manifestghana.com>
2022-03-11 19:04:11 +0000

    Added SMS Module

---
+++ app/Http/Livewire/Sms.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Sms extends Component
+{
+    public function render()
+    {
+        return view('livewire.sms');
+    }
+}
---
+++ resources/views/sms.blade.php
@@ -0,0 +1,8 @@
+<x-backend-layout>  
+    <x-slot name="title"> 
+        EaziProcure &mdash; SMS
+    </x-slot>
+
+    @component('components.sms') @endcomponent 
+
+</x-backend-layout>
