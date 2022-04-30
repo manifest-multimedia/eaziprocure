754544687aa57d674916f5ba32ee25c13b2f0db2
Johnson Sebire <johnson@manifestghana.com>
2022-04-30 06:55:41 +0000

    Updated Auth Layouts

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,28 +1,33 @@
-@extends('layouts.auth')
-@section('title', 'Register')
+<x-neptune-auth-layout>
+<x-slot name='title'> Register </x-slot>
 
-@section('form')
+
+
+<p class="auth-description">Create your free account!<br>Enjoy 14-day free trial.</p>
+
 
         <x-jet-validation-errors class="mb-4" />
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
-            <div>
+            <div class="form-group">
+                <label for="Name" class="mb-2">Enter full name</label>
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" placeholder='Name' required autofocus autocomplete="name" />
             </div>
 
-            <div class="mt-4">
-                
+            <div class="form-group mt-2 mb-2">
+                <label for="Email" class="mb-2 mt-2">Enter valid business email address </label>
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" placeholder="Email" required />
             </div>
 
-            <div class="mt-4">
-                
+            <div class="form-gorup mt-2 mb-2">
+              <label for="password" class="mt-2 mb-2">Enter a secure password</label>  
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" placeholder="Password" required autocomplete="new-password" />
             </div>
 
-            <div class="mt-4">
+            <div class="form-group mt-2 mb-2">
+<label for="password_confirmation" class="mt-2 mb-2">Confirm password</label>
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" placeholder='Password Confirmation' required autocomplete="new-password" />
             </div>
 
@@ -42,17 +47,19 @@
                     </x-jet-label>
                 </div>
             @endif
-
-            <div class="flex items-center justify-end mt-4">
+            <div class="auth-forgot-password float-end form-group mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
-                <x-jet-button class="ml-4">
+                
+            </div>
+                <div class="auth-submit mt-4"><x-jet-button>
                     {{ __('Register') }}
                 </x-jet-button>
             </div>
+            
         </form>
  
 
-@endsection 
+</x-neptune-auth-layout>
