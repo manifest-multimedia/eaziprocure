553c53035946aecfd7cb03566d39cf4e9edd0633
Johnson Sebire <johnson@manifestghana.com>
2022-05-01 22:16:28 +0000

    Updated App Service Provider - Included URL Facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
