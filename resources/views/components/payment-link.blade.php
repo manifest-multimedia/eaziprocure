2afed49eba203dc0b391a6362b73c3f288746a33
Johnson Sebire <johnson@manifestghana.com>
2022-07-01 18:29:46 +0000

    Code Refactored

--- resources/views/components/payment-link.blade.php
+++ resources/views/components/payment-link.blade.php
@@ -1,7 +1,8 @@
     <!-- Simplicity is an acquired taste. - Katharine Gerould -->
     <form method="POST" action="{{ route('pay') }}" accept-charset="UTF-8" class="form-horizontal" role="form">
         <div class="row">
-            
+            @csrf
+            @method('post')
                 <input type="hidden" name="email" value="{{$email}}"> {{-- required --}}
                 <input type="hidden" name="orderID" value="{{$order_id}}">
                 <input type="hidden" name="amount" value="{{$amount}}"> {{-- required in kobo --}}
@@ -12,15 +13,14 @@
                 
                 {{-- <input type="hidden" name="split_code" value="SPL_EgunGUnBeCareful"> to support transaction split. more details https://paystack.com/docs/payments/multi-split-payments/#using-transaction-splits-with-payments --}}
                 {{-- <input type="hidden" name="split" value="{{ json_encode($split) }}"> to support dynamic transaction split. More details https://paystack.com/docs/payments/multi-split-payments/#dynamic-splits --}}
-                {{ csrf_field() }} {{-- works only when using laravel 5.1, 5.2 --}}
+                {{-- {{ csrf_field() }}  --}}
+                {{-- works only when using laravel 5.1, 5.2 --}}
     
                 <input type="hidden" name="_token" value="{{ csrf_token() }}"> {{-- employ this in place of csrf_field only in laravel 5.0 --}}
-    
                
                     <div class="text-center">
                         <button class="btn btn-primary" type="submit">Get Started</button>
                     </div>
-                
          
         </div>
     </form>
